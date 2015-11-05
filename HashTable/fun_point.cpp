a36c7a65c86e70d5784d3058785ed2a39f05e196
Guojian-Chen <chenguojian@gmail.com>
2015-11-04 16:29:02 -0800

    add description for func pointer

--- HashTable/fun_point.cpp
+++ HashTable/fun_point.cpp
@@ -1,4 +1,30 @@
 // A simple C program to show function pointers as parameter
+/*Function Pointers Summary
+
+	Syntax
+
+	Declaring:
+	Declare a function pointer as though you were declaring a function, 
+	except with a name like *foo instead of just foo:
+	void (*foo)(int);
+
+	Initializing
+
+	You can get the address of a function simply by naming it:
+	void foo();
+	func_pointer = foo;
+	
+	or by prefixing the name of the function with an ampersand:
+	void foo();
+	func_pointer = &foo;
+	
+	Invoking
+
+	Invoke the function pointed to just as if you were calling a function.
+	func_pointer( arg1, arg2 );
+	or you may optionally dereference the function pointer before calling the function it points to:
+	(*func_pointer)( arg1, arg2 );
+*/
 #include <stdio.h>
 
 // Two simple functions
